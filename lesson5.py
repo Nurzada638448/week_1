421775a697995d77366a5db828f55d1aa567af63
Nurzada638448 <shermatova960@gmail.com>
2023-04-19 11:07:48 +0600

    list tasks complete

---
+++ lesson5.py
@@ -0,0 +1,64 @@
+# Задача 1
+# numbs = input("Введите список целых чисел через пробел: ").split()
+
+# print("Общее количество элементов в списке:", len(numbs))
+# print("Последний элемент в списке:", numbs[-1])
+# print("Список в обратном порядке:", numbs[::-1])
+
+# if '5' in numbs:
+#     print("Yes")
+# else:
+#     print("No")
+
+# count_fives = numbs.count('5')
+# print("Количество пятерок в списке:", count_fives)
+
+# numbs.pop(0)
+# print("Удален первый элемент в списке" , numbs)
+
+# numbs.pop(-1)
+# print("Удален последний элемент в списке", numbs)
+
+# numbs.sort()
+# print("Оставшие отсортированные элементы" , numbs)
+
+
+
+# Задание 2
+# my_list = [1, 2, 3, 4, 5]
+# my_list.insert(0, 0)
+# print(my_list)
+
+
+
+# Задание 3
+
+# futy = float(input("Введите длину в футах: "))
+# ed_izmer = [
+#     (futy * 12),
+#     (futy / 0.333333),
+#     (futy / 5280),
+#     (futy * 304.8),
+#     (futy * 30.48),
+#     (futy / 0.3048),
+#     (futy / 0.0003048),
+
+# ]
+# izmer = int(input("Введите цифру измерения: "))
+# cifry = [1, 2, 3, 4, 5, 6, 7]
+
+# if cifry[0] == izmer:
+#     print(f'{ed_izmer[0]} дюйм')
+# if cifry[1] == izmer:
+#     print(f'{ed_izmer[1]} ярд')
+# if cifry[2] == izmer:
+#     print(f'{ed_izmer[2]} мили')
+# if cifry[3] == izmer:
+#     print(f'{ed_izmer[3]} миллиметр')
+# if cifry[4] == izmer:
+#     print(f'{ed_izmer[4]} сантиметр')
+# if cifry[5] == izmer:
+#     print(f'{ed_izmer[5]} метр')
+# if cifry[6] == izmer:
+#     print(f'{ed_izmer[6]} км')
+
