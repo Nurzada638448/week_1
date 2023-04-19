94e6a5a86330be2f58b17752a704f519f3ae9264
Nurzada638448 <shermatova960@gmail.com>
2023-04-19 10:55:05 +0600

    first commit

---
+++ llesson1.py
@@ -0,0 +1,36 @@
+# for loop, while loop
+# name = "Nurzada"
+# for i in name:
+#     print(i.upper())
+
+# names = ["Nurzada", "Jarkynai", "Jenya"]
+# for name in names:
+#     print(name)
+
+
+# names = {"name":"Nurzada","name1":"Jarkynai","name2": "Jenya"}
+# for key, value in names.items():
+#     print(key, value)
+
+# names = {"name":"Nurzada","name1":"Jarkynai","name2": "Jenya"}
+# names_replace = {}
+# for key, value in names.items():
+#     names_replace[key] = value.replace("a", "z")
+# print(names_replace)
+
+# list_ = []
+
+
+# for i in range(0,1000):
+#     if i % 3 == 0 and i % 5 == 0:
+#         list_.append(i)
+
+# print(list_)
+
+# num_fact = int(input("Введите номер:"))
+# fact = 1
+# for i in range(1,num_fact + 1):
+#     fact *= i
+# print(fact)
+
+
